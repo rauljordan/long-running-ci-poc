d414d67e61837e956be09f2d6ac4e84633b222cc
Raul Jordan <raul@prysmaticlabs.com>
2025-04-01 10:07:35 -0500

    tests

--- main_test.go
+++ main_test.go
@@ -7,3 +7,9 @@ func TestSomething(t *testing.T) {
 		t.Fatal("add(2, 2) should return 4")
 	}
 }
+
+func TestOther(t *testing.T) {
+	if add(1, 1) != 2 {
+		t.Fatal("add(1, 1) should return 2")
+	}
+}
